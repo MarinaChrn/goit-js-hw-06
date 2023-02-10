90498ec511b1d020aac15ba81260167858804733
MarinaChorna <mrnchrn06@gmail.com>
2023-02-10 23:30:26 +0200

    Завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+//1. Створить окремий елемент <li>. 
+//Обов'язково використовуй метод document.createElement().
+//2. Додасть назву інгредієнта як його текстовий вміст.
+//3. Додасть елементу клас item.
+//4. Після чого, вставить усі 
+//<li> за одну операцію у список ul#ingredients.
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.className = "item";
+  return liEl;
+})
+
+const listIngredientsEl = document.querySelector('ul#ingredients');
+listIngredientsEl.append(...ingredientsEl);
