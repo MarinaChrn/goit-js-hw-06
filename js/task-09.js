187e655cce60fdb920ccd98f9129a4dd3b8b7ad4
MarinaChorna <mrnchrn06@gmail.com>
2023-02-12 09:38:57 +0200

    Завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Напиши скрипт, який змінює кольори фону елемента <body> 
+// через інлайн-стиль по кліку на button.change-color
+// і виводить значення кольору в span.color.
+
+
+//console.log(color);
+const btnEl = document.querySelector('button.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('span.color');
+
+btnEl.addEventListener('click', (event)=>{
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = `${color}`;
+})
